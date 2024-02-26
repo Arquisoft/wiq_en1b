5fe7f8d4ad175afb69db137f5d331f9337c90b3f
Mister-Mario <mariojr2003@gmail.com>
2024-02-26 20:32:52 +0100

    Configured the packages and files needed to start the backend development

---
+++ backend/wiq/src/test/java/com/wiq/wiq/WiqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiq.wiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
