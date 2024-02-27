f4ccd628e28cb1ed472c55a26ad51223f1a7d4e2
lauratbg <UO275725@uniovi.es>
2024-02-27 18:39:51 +0100

    First version of Home View. Modified it to be the first view, redirections to login and registration views

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,36 +1,23 @@
-import React, { useState } from 'react';
-import AddUser from './components/AddUser';
-import Login from './components/Login';
+import Home from './components/Home';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
+
+
+function Navbar() {
+  return (
+      <Typography variant="h6" gutterBottom>
+        Know and Win!
+      </Typography>
+  );
+}
 
 function App() {
-  const [showLogin, setShowLogin] = useState(true);
-
-  const handleToggleView = () => {
-    setShowLogin(!showLogin);
-  };
-
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
-      <Typography component="h1" variant="h5" align="center" sx={{ marginTop: 2 }}>
-        Welcome to the 2024 edition of the Software Architecture course
-      </Typography>
-      {showLogin ? <Login /> : <AddUser />}
-      <Typography component="div" align="center" sx={{ marginTop: 2 }}>
-        {showLogin ? (
-          <Link name="gotoregister" component="button" variant="body2" onClick={handleToggleView}>
-            Don't have an account? Register here.
-          </Link>
-        ) : (
-          <Link component="button" variant="body2" onClick={handleToggleView}>
-            Already have an account? Login here.
-          </Link>
-        )}
-      </Typography>
+      <Navbar /> 
+      <Home />
     </Container>
   );
 }
