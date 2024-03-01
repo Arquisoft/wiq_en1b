b8d485118ca89e00de186d6146b931c9a0819912
lauratbg <UO275725@uniovi.es>
2024-03-01 10:00:09 +0100

    First modifications of login

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,6 +1,9 @@
 import Home from './components/Home/Home';
 import Container from '@mui/material/Container';
 import Navbar from './components/fragments/NavBar'; 
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Login from './components/Login';
+import AddUser from './components/AddUser';
 
 
 function App() {
--- webapp/src/components/fragments/NavBar.js
+++ webapp/src/components/fragments/NavBar.js
@@ -1,6 +1,6 @@
 import Typography from "@mui/material/Typography";
 import "./NavBar.css";
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 function Navbar() {
 
