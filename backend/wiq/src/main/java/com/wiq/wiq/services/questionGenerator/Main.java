3520d5f3fd1dfc52c2d39178bca38726a9a3c8a6
jorge <uo289845@uniovi.es>
2024-03-02 12:11:23 +0100

    Make it so there are three examples per question type

--- backend/wiq/src/main/java/com/wiq/wiq/services/questionGenerator/Main.java
+++ backend/wiq/src/main/java/com/wiq/wiq/services/questionGenerator/Main.java
@@ -8,11 +8,22 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		QuestionGenerator qg = new QuestionGenerator();
-		String json = "";
-		json = qg.generateQuestion(QuestionType.POPULATION);
-		// json = qg.generateQuestion(QuestionType.CAPITAL);
-		System.out.println(json);
+//		String json = "";
+////		json = qg.generateQuestion(QuestionType.POPULATION);
+//		json = qg.generateQuestion(QuestionType.CAPITAL);
+//		System.out.println(json);
+		run(qg);
 
+	}
+	
+	private static void run(QuestionGenerator qg) {
+		for(int i=0; i<3; i++) {
+			System.out.println(qg.generateQuestion(QuestionType.POPULATION));
+		}
+		System.out.println();
+		for(int i=0; i<3; i++) {
+			System.out.println(qg.generateQuestion(QuestionType.CAPITAL));
+		}
 	}
 
 }
--- backend/wiq/src/main/java/com/wiq/wiq/services/questionGenerator/QuestionGenerator.java
+++ backend/wiq/src/main/java/com/wiq/wiq/services/questionGenerator/QuestionGenerator.java
@@ -12,6 +12,9 @@ public class QuestionGenerator {
 	private AbstractGenerator generator;
 	private String id;
 	
+	private static String[] POP_ENTITIES = {"Q14317", "Q12273", "Q14649"};
+	private static String[] CAP_ENTITIES = {"Q3934", "Q29", "Q43"};
+	
 	public String generateQuestion(QuestionType type) {
 		generatorFactory(type);
 		String answer = generator.generate(id).getJSON().toString();
@@ -21,16 +24,26 @@ public class QuestionGenerator {
 	private void generatorFactory(QuestionType type) {
 		switch (type) {
 			case POPULATION: {
-				id = "Q14317";
+				id = POP_ENTITIES[0];
 				generator = new PopulationGenerator();
+				moveUp(POP_ENTITIES);
+				POP_ENTITIES[POP_ENTITIES.length-1] = id;
 				break;
 			}
 			case CAPITAL: {
-				id = "Q3934";
+				id = CAP_ENTITIES[0];
 				generator = new CapitalGenerator();
+				moveUp(CAP_ENTITIES);
+				CAP_ENTITIES[CAP_ENTITIES.length-1] = id;
 				break;
 				
 			}
+		}
+	}
+	
+	private void moveUp(String[] list) {
+		for(int i=0; i<list.length-1; i++) {
+			list[i] = list[i+1];
 		}
 	}
 
