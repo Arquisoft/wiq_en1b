520fafb8038f8405663bdc134462a10d643d7613
Mister-Mario <mariojr2003@gmail.com>
2024-03-04 17:29:13 +0100

    Everything works: -Quetion generator need to run Main.java inside wiq_en1b\backend\wiq\src\main\java\com\wiq\wiq\services\questionGenerator To test the webapp and the backend -Run SpringBoot with the VSCode Extension and class WIQApplication.java inside wiq_en1b\backend\wiq\src\main\java\com\wiq\wiq -Go in console to webapp dir, then do npm install and the npm start

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Container from '@mui/material/Container';
 import QuestionView from './components/questionView/QuestionView';
-import Navbar from './components/fragments/NavBar';
-import Home from './components/home/Home';
-import Login from './components/Login';
-import AddUser from './components/AddUser';
-import Instructions from './components/Instructions'; 
 
 
 function App() {
   return (
+    /*
     <Router>
       <Container component="main" maxWidth="xl">
         <Navbar />
@@ -24,6 +18,8 @@ function App() {
         </Routes>
       </Container>
     </Router>
+    */
+   <QuestionView />
   );
 }
 
