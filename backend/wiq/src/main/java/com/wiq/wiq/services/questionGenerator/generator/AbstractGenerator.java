aec32189f8b4caa9939d6a9682681a37be425730
jorge <uo289845@uniovi.es>
2024-03-05 18:21:14 +0100

    Fixed an issue with the path to the messages
    The path was wrong and it gave an error whenever it try access the messages files

--- backend/wiq/src/main/java/com/wiq/wiq/services/questionGenerator/generator/AbstractGenerator.java
+++ backend/wiq/src/main/java/com/wiq/wiq/services/questionGenerator/generator/AbstractGenerator.java
@@ -30,7 +30,8 @@ public abstract class AbstractGenerator {
 	public AbstractGenerator(String propertyId) {
 		this.propertyId = propertyId;
 		localization = Locale.ENGLISH;
-		this.messages = ResourceBundle.getBundle("rsc/messages", localization);
+		this.messages = ResourceBundle.getBundle("com/wiq/wiq/services/questionGenerator/"+
+		"generator/rsc/messages", localization);
 	}
 	
 	/**
