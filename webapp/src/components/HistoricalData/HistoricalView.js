5c3f676f241f09a290545df2d5e226238f8709df
uo289267 <uo289267@uniovi.es>
2024-03-27 12:30:23 +0100

    Historical individual elements created

--- webapp/src/components/HistoricalData/HistoricalView.js
+++ webapp/src/components/HistoricalData/HistoricalView.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 export default function HistoricalView() {
       return (
-        <div>
+        <div className='globalHistoricalView'>
           <HistoricalGameElement  />
+          <br />
+          <HistoricalGameElement  />
+          <br />
           <HistoricalGameElement  />
         </div>
       )
@@ -19,10 +22,10 @@ function HistoricalGameElement(){
       <button className="historicalButton" onClick={handleClick}>Toggle State</button>
 
       <ul style={{ display: toggle ? 'block' : 'none' }}>
-        <li >An item</li>
+        <li>An item</li>
         <li>A second item</li>
-        <li >A third item</li>
-        <li >A fourth item</li>
+        <li>A third item</li>
+        <li>A fourth item</li>
         <li>And a fifth one</li>
       </ul>
 
