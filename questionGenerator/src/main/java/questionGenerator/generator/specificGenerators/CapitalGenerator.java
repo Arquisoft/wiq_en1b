41940581f154ec0c62229b145d81a4dd31e39eef
jorge <uo289845@uniovi.es>
2024-03-30 17:24:33 +0100

    Removed commented code and unused imports

--- questionGenerator/src/main/java/questionGenerator/generator/specificGenerators/CapitalGenerator.java
+++ questionGenerator/src/main/java/questionGenerator/generator/specificGenerators/CapitalGenerator.java
@@ -1,9 +1,5 @@
 package main.java.questionGenerator.generator.specificGenerators;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 import main.java.questionGenerator.generator.RightAnswerIsEntity;
 
 public class CapitalGenerator extends RightAnswerIsEntity {
@@ -19,25 +15,5 @@ public class CapitalGenerator extends RightAnswerIsEntity {
 		String q = getMessages().getString("question.capital");
 		return String.format(q, name);
 	}
-
-	// @Override
-	// protected List<String> getWrongAnswers(String rightAnswer) {
-	// 	Random rnd = new Random();
-	// 	String[] entities = {"Q142", "Q183", "Q16", "Q142", "Q30", "Q408", "Q668", "Q17", "Q38", "Q159",
-	// 	 "Q79", "Q155", "Q884", "Q414", "Q41", "Q258", "Q96", "Q843", "Q148", "Q20"};
-	// 	List<String> result = new ArrayList<>();
-	// 	List<Integer> used = new ArrayList<>();
-	// 	for(int i = 0; i < 3; i++){
-	// 			int rndnum = rnd.nextInt(entities.length);
-	// 			String wrong = getAnswer(entities[rndnum]);
-	// 		if(wrong.equals(rightAnswer) || used.contains(rndnum))
-	// 			i--;
-	// 		else{
-	// 			result.add(wrong);
-	// 			used.add(rndnum);
-	// 		}
-	// 	}
-	// 	return result;
-	// }
 
 }
