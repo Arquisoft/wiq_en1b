1292f73787476db8b577f40b43fdef569ca7a3c5
jorge <uo289845@uniovi.es>
2024-04-02 16:30:59 +0200

    Now it ensures that the language chosen is the correct one

--- questionGenerator/src/main/java/questionGenerator/generator/specificGenerators/LanguageGenerator.java
+++ questionGenerator/src/main/java/questionGenerator/generator/specificGenerators/LanguageGenerator.java
@@ -1,5 +1,12 @@
 package main.java.questionGenerator.generator.specificGenerators;
 
+import java.util.List;
+import java.util.Map;
+
+import org.wikidata.wdtk.datamodel.interfaces.Snak;
+import org.wikidata.wdtk.datamodel.interfaces.SnakGroup;
+import org.wikidata.wdtk.datamodel.interfaces.Statement;
+
 import main.java.questionGenerator.generator.RightAnswerIsEntity;
 import main.java.questionGenerator.question.QuestionType;
 
@@ -15,6 +22,28 @@ public class LanguageGenerator extends RightAnswerIsEntity {
 	protected String getQuestion(String name) {
 		String q = getMessages().getString("question.language");
 		return String.format(q, name);
+	}
+
+	@Override
+	protected String getRightAnswer(Map<String, List<Statement>> claims) {
+		for(Statement st : claims.get(super.getPropertyId())) {
+			boolean valid = true;
+			for(SnakGroup sg : st.getQualifiers()) {
+				for(Snak s : sg.getSnaks()) {
+					String value = getRightAnswerEntity(s.getPropertyId().toString());
+					if(value.equals("P518")) {
+						valid = false;
+						break;
+					}
+				}
+				if(!valid)
+					break;
+			}
+			if(valid) {
+				return processRightAnswer(st);
+			}
+		}
+		return null;
 	}
 
 }
