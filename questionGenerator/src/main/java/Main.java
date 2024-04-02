5e8dcaac9ce97e2747e1390748335eceff80f8f3
jorge <uo289845@uniovi.es>
2024-04-02 15:32:25 +0200

    Updated the main so now it actually saves the questions

--- questionGenerator/src/main/java/Main.java
+++ questionGenerator/src/main/java/Main.java
@@ -5,32 +5,32 @@ import java.util.List;
 import main.java.questionGenerator.QuestionGenerator;
 import main.java.questionGenerator.question.Question;
 import main.java.questionGenerator.question.QuestionType;
+import main.java.questionGenerator.repository.QuestionRepository;
 
 public class Main {
 
     public static void main(String[] args) {
 		QuestionGenerator qg = new QuestionGenerator("en");
-		List<Question> qs = qg.generateQuestions(QuestionType.CAPITAL, 3);
-		for(Question q : qs) {
-			System.out.println(q.getJSON().toString());
-		}
+		
+		run(qg, QuestionType.CAPITAL, 3);
 		System.out.println();
 		
-		qs = qg.generateQuestions(QuestionType.LANGUAGE, 3);
-		for(Question q : qs) {
-			System.out.println(q.getJSON().toString());
-		}
+		run(qg, QuestionType.LANGUAGE, 3);
 		System.out.println();
 		
-		qs = qg.generateQuestions(QuestionType.POPULATION, 3);
-		for(Question q : qs) {
-			System.out.println(q.getJSON().toString());
-		}
+		run(qg, QuestionType.POPULATION, 3);
 		System.out.println();
 		
-		qs = qg.generateQuestions(QuestionType.SIZE, 3);
-		for(Question q : qs) {
-			System.out.println(q.getJSON().toString());
-		}
+		run(qg, QuestionType.SIZE, 3);
+	}
+	
+	private static void run(QuestionGenerator qg, QuestionType type, int numberOfQuestions){
+			List<Question> questions = qg.generateQuestions(type, numberOfQuestions);
+			for(int i=0; i<questions.size(); i++) {
+				Question question = questions.get(i);
+				question.setNumber(i);
+				System.out.println(question.getJSON().toString());
+			}
+			QuestionRepository.getInstance().insert(questions.stream().map(q -> q.getJSON().toString()).toList());
 	}
 }
