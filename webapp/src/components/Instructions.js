fd88fd5bb25ba646e53506ce2fb77263bbbdb14d
lauratbg <UO275725@uniovi.es>
2024-04-09 16:23:28 +0200

    First idea of navegability. Added link to the logo

--- webapp/src/components/Instructions.js
+++ webapp/src/components/Instructions.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../custom.css';
 import {useTranslation} from "react-i18next";
+import { Link } from "react-router-dom";
+
 
 
 
@@ -10,6 +12,7 @@ function Instructions() {
 
   return (
     <div className='instrucions_container'>
+      
       <h1 className='instructions_title'>{t("instructions.title")}</h1>
      <section>   
       <article>
@@ -57,7 +60,29 @@ function Instructions() {
             </article>
            
       </section>
+      {/* <Link to="/Home" className="buttonb" variant="body2">
+          <button class="button-back">
+            
+  <div class="button-box">
+    <span class="button-elem">
+      <svg viewBox="0 0 46 40" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M46 20.038c0-.7-.3-1.5-.8-2.1l-16-17c-1.1-1-3.2-1.4-4.4-.3-1.2 1.1-1.2 3.3 0 4.4l11.3 11.9H3c-1.7 0-3 1.3-3 3s1.3 3 3 3h33.1l-11.3 11.9c-1 1-1.2 3.3 0 4.4 1.2 1.1 3.3.8 4.4-.3l16-17c.5-.5.8-1.1.8-1.9z"
+        ></path>
+      </svg>
+    </span>
+    <span class="button-elem">
+      <svg viewBox="0 0 46 40">
+        <path
+          d="M46 20.038c0-.7-.3-1.5-.8-2.1l-16-17c-1.1-1-3.2-1.4-4.4-.3-1.2 1.1-1.2 3.3 0 4.4l11.3 11.9H3c-1.7 0-3 1.3-3 3s1.3 3 3 3h33.1l-11.3 11.9c-1 1-1.2 3.3 0 4.4 1.2 1.1 3.3.8 4.4-.3l16-17c.5-.5.8-1.1.8-1.9z"
+        ></path>
+      </svg>
+    </span>
+  </div>
+</button>
+            </Link> */}
     </div>
+    
   );
 }
 
--- webapp/src/components/loginAndRegistration/Login.js
+++ webapp/src/components/loginAndRegistration/Login.js
@@ -29,10 +29,14 @@ const Login = () => {
 
   return (
     <div className="general">
-
+         
     <div className="card">
+      
       <div className="card2">
+        
         <form className="form" onSubmit={handleSubmit}>
+       
+
           <h1 className="title-login">{t("login.title")}</h1>
           <div className="input-box">
               <input
@@ -59,9 +63,31 @@ const Login = () => {
               <input type="checkbox" /> {t("login.remember_me")}
             </label>
           </div>
-
           <button type="submit">{t("login.login_button")}</button>
+          
           <LinkRegister />
+          <Link to="/Home" className="buttonb" variant="body2">
+          <button class="button-back">
+            
+  <div class="button-box">
+    <span class="button-elem">
+      <svg viewBox="0 0 46 40" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M46 20.038c0-.7-.3-1.5-.8-2.1l-16-17c-1.1-1-3.2-1.4-4.4-.3-1.2 1.1-1.2 3.3 0 4.4l11.3 11.9H3c-1.7 0-3 1.3-3 3s1.3 3 3 3h33.1l-11.3 11.9c-1 1-1.2 3.3 0 4.4 1.2 1.1 3.3.8 4.4-.3l16-17c.5-.5.8-1.1.8-1.9z"
+        ></path>
+      </svg>
+    </span>
+    <span class="button-elem">
+      <svg viewBox="0 0 46 40">
+        <path
+          d="M46 20.038c0-.7-.3-1.5-.8-2.1l-16-17c-1.1-1-3.2-1.4-4.4-.3-1.2 1.1-1.2 3.3 0 4.4l11.3 11.9H3c-1.7 0-3 1.3-3 3s1.3 3 3 3h33.1l-11.3 11.9c-1 1-1.2 3.3 0 4.4 1.2 1.1 3.3.8 4.4-.3l16-17c.5-.5.8-1.1.8-1.9z"
+        ></path>
+      </svg>
+    </span>
+  </div>
+</button>
+            </Link>
+          
         </form>
       </div>
     </div>
@@ -77,6 +103,8 @@ function LinkRegister() {
     </Link>
   );
 }
+
+
 
 export default Login;
 
