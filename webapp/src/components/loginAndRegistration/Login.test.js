62d7d76c3e6a9fa1ad1f14db67e15e3854f6ec90
Mister-Mario <mariojr2003@gmail.com>
2024-04-14 23:39:50 +0200

    Added new test case for code cover

--- webapp/src/components/loginAndRegistration/Login.test.js
+++ webapp/src/components/loginAndRegistration/Login.test.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import Login from './Login';
 import { BrowserRouter as Router } from 'react-router-dom'; 
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
 
 // Mocking useTranslation hook
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({ t: key => key }),
 }));
 
+const mockAxios = new MockAdapter(axios);
+
 describe('<Login />', () => {
-  test('renders the Login component', () => {
+  it('renders the Login component', () => {
     render(
         <Router>
           <Login />
@@ -21,5 +25,25 @@ describe('<Login />', () => {
     expect(screen.getByPlaceholderText('login.password_placeholder')).toBeInTheDocument();
     expect(screen.getByText('login.login_button')).toBeInTheDocument();
     expect(screen.getByText('login.register_link')).toBeInTheDocument();
+  });
+
+  it('sends a correct log in petition', async () => {
+    mockAxios.onPost('http://localhost:8000/login').reply(200, { token: "token", username: "username"});
+
+    render(
+      <Router>
+        <Login />
+      </Router>
+  );
+
+  fireEvent.change(screen.getByPlaceholderText('login.username_placeholder'), { target: { value: 'user' } });
+  fireEvent.change(screen.getByPlaceholderText('login.password_placeholder'), { target: { value: 'j' } });
+  fireEvent.click(screen.getByText('login.login_button'));
+
+  // Wait for redirection to happen
+  await waitFor(() => {
+    expect(window.location.pathname).toBe('/menu');
+  });
+
   });
 });
