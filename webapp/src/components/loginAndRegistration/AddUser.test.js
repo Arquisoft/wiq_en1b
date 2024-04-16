46c3e9293c120dcb97fec83ff9d78633eaadaaac
sinne10 <daniel.sinne2002@gmail.com>
2024-04-16 16:59:22 +0200

    Fixed duplicated code

--- webapp/src/components/loginAndRegistration/AddUser.test.js
+++ webapp/src/components/loginAndRegistration/AddUser.test.js
@@ -14,12 +14,15 @@ jest.mock('axios');
 
 describe('<AddUser />', () => {
 
-  test('renders the AddUser component', () => {
+  beforeEach(() => {
     render(
       <Router>
         <AddUser />
       </Router>
     );
+  });
+
+  test('renders the AddUser component', () => {
 
     expect(screen.getByText('addUser.title')).toBeInTheDocument();
     expect(screen.getByText('addUser.username_placeholder:')).toBeInTheDocument();
@@ -30,147 +33,52 @@ describe('<AddUser />', () => {
 
   });
 
-  test('displays error message when passwords do not match', () => {
-    render(
-      <Router>
-        <AddUser />
-      </Router>
-    );
-
+  const fillFormAndSubmit = (username, password, repeatPassword) => {
     const usernameInput = screen.getByPlaceholderText('addUser.username_placeholder');
-    fireEvent.change(usernameInput, { target: { value: 'username' } });
+    fireEvent.change(usernameInput, { target: { value: username } });
 
     const passwordInput = screen.getByPlaceholderText('addUser.password_placeholder');
-    fireEvent.change(passwordInput, { target: { value: '12345678' } });
+    fireEvent.change(passwordInput, { target: { value: password } });
 
     const repeatPasswordInput = screen.getByPlaceholderText('addUser.repeat_password_placeholder');
-    fireEvent.change(repeatPasswordInput, { target: { value: '123456789' } });
+    fireEvent.change(repeatPasswordInput, { target: { value: repeatPassword } });
 
     const submitButton = screen.getByText('addUser.register_button');
     fireEvent.click(submitButton);
+  };
 
+  test('displays error message when passwords do not match', () => {
+    fillFormAndSubmit('username', '12345678', '123456789');
     expect(screen.getByText('addUser.error_passwords_no_match')).toBeInTheDocument();
   });
 
   test('displays error message when spaces in password', () => {
-    render(
-      <Router>
-        <AddUser />
-      </Router>
-    );
-
-    const usernameInput = screen.getByPlaceholderText('addUser.username_placeholder');
-    fireEvent.change(usernameInput, { target: { value: 'username' } });
-
-    const passwordInput = screen.getByPlaceholderText('addUser.password_placeholder');
-    fireEvent.change(passwordInput, { target: { value: '1234 5678' } });
-
-    const repeatPasswordInput = screen.getByPlaceholderText('addUser.repeat_password_placeholder');
-    fireEvent.change(repeatPasswordInput, { target: { value: '1234 5678' } });
-
-    const submitButton = screen.getByText('addUser.register_button');
-    fireEvent.click(submitButton);
-
+    fillFormAndSubmit('username', '1234 5678', '1234 5678');
     expect(screen.getByText('addUser.error_password_spaces')).toBeInTheDocument();
   });
 
   test('displays error message when passwords too short', () => {
-    render(
-      <Router>
-        <AddUser />
-      </Router>
-    );
-
-    const usernameInput = screen.getByPlaceholderText('addUser.username_placeholder');
-    fireEvent.change(usernameInput, { target: { value: 'username' } });
-
-    const passwordInput = screen.getByPlaceholderText('addUser.password_placeholder');
-    fireEvent.change(passwordInput, { target: { value: '1234567' } });
-
-    const repeatPasswordInput = screen.getByPlaceholderText('addUser.repeat_password_placeholder');
-    fireEvent.change(repeatPasswordInput, { target: { value: '1234567' } });
-
-    const submitButton = screen.getByText('addUser.register_button');
-    fireEvent.click(submitButton);
-
+    fillFormAndSubmit('username', '1234567', '1234567');
     expect(screen.getByText('addUser.error_password_minimum_length')).toBeInTheDocument();
   });
 
   test('displays error message when passwords too long', () => {
-    render(
-      <Router>
-        <AddUser />
-      </Router>
-    );
-
-    const usernameInput = screen.getByPlaceholderText('addUser.username_placeholder');
-    fireEvent.change(usernameInput, { target: { value: 'username' } });
-
-    const passwordInput = screen.getByPlaceholderText('addUser.password_placeholder');
-    fireEvent.change(passwordInput, { target: { value: '01234567890123456789012345678901234567890123456789012345678901234' } });
-
-    const repeatPasswordInput = screen.getByPlaceholderText('addUser.repeat_password_placeholder');
-    fireEvent.change(repeatPasswordInput, { target: { value: '01234567890123456789012345678901234567890123456789012345678901234' } });
-
-    const submitButton = screen.getByText('addUser.register_button');
-    fireEvent.click(submitButton);
-
+    fillFormAndSubmit('username', '01234567890123456789012345678901234567890123456789012345678901234', '01234567890123456789012345678901234567890123456789012345678901234');
     expect(screen.getByText('addUser.error_password_maximum_length')).toBeInTheDocument();
   });
 
   test('displays error message when spaces in username', () => {
-    render(
-      <Router>
-        <AddUser />
-      </Router>
-    );
-
-    const usernameInput = screen.getByPlaceholderText('addUser.username_placeholder');
-    fireEvent.change(usernameInput, { target: { value: 'user name' } });
-
-    const passwordInput = screen.getByPlaceholderText('addUser.password_placeholder');
-    fireEvent.change(passwordInput, { target: { value: '12345678' } });
-
-    const repeatPasswordInput = screen.getByPlaceholderText('addUser.repeat_password_placeholder');
-    fireEvent.change(repeatPasswordInput, { target: { value: '12345678' } });
-
-    const submitButton = screen.getByText('addUser.register_button');
-    fireEvent.click(submitButton);
-
+    fillFormAndSubmit('user name', '12345678', '12345678');
     expect(screen.getByText('addUser.error_username_spaces')).toBeInTheDocument();
   });
 
   test('displays error message when username is already in use', async () => {
-
-    // Mock axios post method to simulate response for username already in use
     axios.post.mockRejectedValue({ response: { data: { error: 'Username already in use' } } });
-
-    render(
-      <Router>
-        <AddUser />
-      </Router>
-    );
-
-    const usernameInput = screen.getByPlaceholderText('addUser.username_placeholder');
-    fireEvent.change(usernameInput, { target: { value: 'existing_user' } });
-
-    const passwordInput = screen.getByPlaceholderText('addUser.password_placeholder');
-    fireEvent.change(passwordInput, { target: { value: '12345678' } });
-
-    const repeatPasswordInput = screen.getByPlaceholderText('addUser.repeat_password_placeholder');
-    fireEvent.change(repeatPasswordInput, { target: { value: '12345678' } });
-
-    const submitButton = screen.getByText('addUser.register_button');
-    fireEvent.click(submitButton);
-
-    // Wait for the asynchronous axios call to be completed
+    fillFormAndSubmit('existing_user', '12345678', '12345678');
     await waitFor(() => {
       expect(screen.getByText('addUser.error_username_in_use')).toBeInTheDocument();
     });
-
-    // Ensure axios.post is called with the correct data
     expect(axios.post).toHaveBeenCalledWith(expect.any(String), { username: 'existing_user', password: '12345678' });
-
   });
 
 });
