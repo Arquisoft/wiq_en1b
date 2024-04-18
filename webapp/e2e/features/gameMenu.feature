8c0b47f31d9d86525295c7a7c257506d19b75571
uo289267 <uo289267@uniovi.es>
2024-04-18 13:24:19 +0200

    Added gameMenu tests to e2e

---
+++ webapp/e2e/features/gameMenu.feature
@@ -0,0 +1,4 @@
+Feature: Game Menu page functionality
+ Scenario: There should be visible three links
+    Given I am on the game menu
+    Then three buttons should be visible
---
+++ webapp/e2e/steps/gameMenu.steps.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+const { defineFeature, loadFeature } = require('jest-cucumber');
+const setDefaultOptions = require('expect-puppeteer').setDefaultOptions;
+
+const feature = loadFeature('./features/gameMenu.feature');
+
+let page;
+let browser;
+
+defineFeature(feature, test => {
+  
+  beforeAll(async () => {
+      browser = await puppeteer.launch({
+      slowMo: 20,
+      defaultViewport: { width: 1920, height: 1080 },
+      args: ['--window-size=1920,1080']
+    });
+     
+    page = await browser.newPage();
+    setDefaultOptions({ timeout: 10000 });
+  });
+
+  test('There should be visible three links', ({ given, then }) => {
+    given('I am on the the game menu', async () => {
+      await page.goto('http://localhost:3000/menu'); 
+      await page.waitForSelector('.divMenu');
+    });
+
+    then('Then three buttons should be visible', async () => {
+      //await expect(page).toMatchElement('.linkButton');
+      const elements = await page.$$('.linkButton');
+      expect(elements.length).toBeGreaterThan(0); // At least one element with class 'linkButton'
+    });
+  });
+
+});
