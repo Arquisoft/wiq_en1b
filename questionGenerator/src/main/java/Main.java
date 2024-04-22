30f56e97df71968bc69d238651c668497029b3e0
jorge <uo289845@uniovi.es>
2024-04-22 17:31:46 +0200

    Updated the main naming nomenclature

--- questionGenerator/src/main/java/Main.java
+++ questionGenerator/src/main/java/Main.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import main.java.questionGenerator.QuestionGenerator;
@@ -8,31 +9,54 @@ import main.java.questionGenerator.question.QuestionType;
 import main.java.questionGenerator.repository.QuestionRepository;
 
 public class Main {
+	
+	private static QuestionGenerator qg = QuestionGenerator.getInstance();
+	
+	// private static final long TIME_SKIP = 18000000; //5 hours
+	// private static final long TIME_SKIP = 43200000; //12 hours
+	private static final long TIME_SKIP = 1000; //1 minute
+	
 
-    public static void main(String[] args) {
-		QuestionGenerator qg = QuestionGenerator.getInstance();
-		
-		run(qg, QuestionType.CAPITAL, 3);
-		run(qg, QuestionType.LANGUAGE, 3);
-		run(qg, QuestionType.POPULATION, 3);
-		run(qg, QuestionType.SIZE, 3);
-		run(qg, QuestionType.HEAD_OF_GOVERMENT, 3);
-		run(qg, QuestionType.DIRECTOR, 3);
-		run(qg, QuestionType.VIDEOGAME_DEVELOPER, 3);
-		run(qg, QuestionType.VIDEOGAME_PUBLISHER, 3);
-		run(qg, QuestionType.VIDEOGAME_GENRE, 3);
-		run(qg, QuestionType.VIDEOGAME_COUNTRY, 3);
+	private static String[] languages = {"en", "es", "tr"};
 
+	private static QuestionType[] types = {QuestionType.CAPITAL, QuestionType.LANGUAGE, QuestionType.POPULATION, 
+		QuestionType.SIZE, QuestionType.HEAD_OF_GOVERMENT, QuestionType.VIDEOGAME_DEVELOPER, 
+		QuestionType.VIDEOGAME_PUBLISHER, QuestionType.VIDEOGAME_GENRE, QuestionType.VIDEOGAME_COUNTRY};
+
+ 	// private static final int NUMBER_OF_QUESTIONS = 50;
+	// private static final int NUMBER_OF_QUESTIONS = 100;
+	// private static final int NUMBER_OF_QUESTIONS = 3;
+	private static final int NUMBER_OF_QUESTIONS = 1;
+
+	public static void main(String[] args) {
+		while(true) {
+			List<String> questions = generate().stream().map(q -> q.getJSON().toString()).toList();
+			QuestionRepository.getInstance().populate(questions);
+			try {
+				Thread.sleep(TIME_SKIP);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
-	private static void run(QuestionGenerator qg, QuestionType type, int numberOfQuestions){
-			List<Question> questions = qg.generateQuestions(type, numberOfQuestions);
-			for(int i=0; i<questions.size(); i++) {
-				Question question = questions.get(i);
-				question.setNumber(i);
-				System.out.println(question.getJSON().toString());
-			}
-			System.out.println();
-			QuestionRepository.getInstance().insert(questions.stream().map(q -> q.getJSON().toString()).toList());
+	private static List<Question> generate() {
+		List<Question> questions = new ArrayList<Question>();
+		for(String lang : languages) {
+			qg.setLanguageCode(lang);
+			for(QuestionType type: types)
+				questions.addAll(run(qg, type, NUMBER_OF_QUESTIONS));
+		}
+		return questions;
 	}
+	
+	private static List<Question> run(QuestionGenerator qg, QuestionType type, int numberOfQuestions) {
+		List<Question> questions = qg.generateQuestions(type, numberOfQuestions);
+		for(int i=0; i<questions.size(); i++) {
+			Question question = questions.get(i);
+			question.setNumber(i);
+		}
+		return questions;
+	}
+
 }
