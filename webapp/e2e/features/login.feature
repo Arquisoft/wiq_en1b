65d7bc37c4915e2c1b5ad8a675e1b6648718b477
lauratbg <UO275725@uniovi.es>
2024-04-26 15:55:34 +0200

    First try of login test

---
+++ webapp/e2e/features/login.feature
@@ -0,0 +1,6 @@
+Feature: Login page functionality
+
+  Scenario: Login
+    Given I am on the login page
+    I login as user
+    I am in /menu
---
+++ webapp/e2e/steps/login.steps.js
@@ -0,0 +1,52 @@
+const puppeteer = require('puppeteer');
+const { defineFeature, loadFeature } = require('jest-cucumber');
+const setDefaultOptions = require('expect-puppeteer').setDefaultOptions;
+
+const feature = loadFeature('./features/login.feature');
+
+const { register, login, logout } = require("../utils");
+
+let page;
+let browser;
+
+const email = "testUser1@example.com";
+const username = "testUser1"
+const password = "testUserPassword"
+
+defineFeature(feature, test => {
+  
+    beforeAll(async () => {
+        browser = await puppeteer.launch({
+          headless: "new",
+          slowMo: 40,
+          defaultViewport: { width: 1920, height: 1080 },
+          args: ['--window-size=1920,1080']
+      });
+       
+      page = await browser.newPage();
+      setDefaultOptions({ timeout: 30000 });
+  
+      await register(page, email, username, password);
+    });
+
+  beforeEach(async () => {
+    await logout(page);
+    await login(page, username, password);
+  })
+
+  test('Login', ({ given,when, then }) => {
+    given('I am on the login page', async () => {
+      await page.goto('http://localhost:3000/login'); 
+      await page.waitForSelector('.general');
+    });
+    when('I login as user', async () => {
+        await login(page, email, username, password);
+    });
+    then('I am in /menu', async () => {
+      await expect(page).toMatchElement('.divMenu');
+    });
+  });
+  
+  
+
+});
