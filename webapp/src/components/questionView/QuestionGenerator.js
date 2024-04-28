e6ef9545385daf8923019f5a96582dc9f3c9a590
Mister-Mario <mariojr2003@gmail.com>
2024-04-28 02:45:38 +0200

    Removed unused comments

--- webapp/src/components/questionView/QuestionGenerator.js
+++ webapp/src/components/questionView/QuestionGenerator.js
@@ -8,34 +8,7 @@ class QuestionGenerator{
         
     }
 
-    async generateQuestions(lang, type, amount, token) {
-        
-        // try {
-        //     //const response = await fetch(this.apiUrl);
-        //     //const receivedQuestions = await response.json();
-
-        //     //Mockup
-        //     console.log("type: "+type+" amount: "+amount)
-        //     const receivedQuestions = JSON.parse('{"0":{"question":"What is the population of Oviedo?","answers":["225089","191325","220587","121548"]},'+
-        //     '"1":{"question":"¿Cuál es la población de Gijón?","answers":["275274","159658","233982","305554"]},'+
-        //     '"2":{"question":"¿Cuál es la población de Avilés?","answers":["82568","115595","41284","122200"]},'+
-        //     '"3":{"question":"¿Cuál es la capital de Asturias?","answers":["Ciudad de Oviedo","a","b","c"]},'+
-        //     '"4":{"question":"¿Cuál es la capital de España?","answers":["Madrid","a","b","c"]},'+
-        //     '"5":{"question":"¿Cuál es la capital de Turquía?","answers":["Ankara","a","b","c"]}}')
-            
-        //     let i = 0;
-        //     var questions = [];
-        //     for (const key in receivedQuestions) {
-        //         questions[i] = new Question(receivedQuestions[key]);
-        //         i += 1;
-        //     }
-        //     console.log(questions);
-        //     return questions;
-        // } catch (error) {
-        //     throw new Error(error);
-        // }
-        
-        
+    async generateQuestions(lang, type, amount, token) {     
         
         try {
             let response;
